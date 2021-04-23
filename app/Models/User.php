8002c20c647881478e5bd988d97eaf21324a33ae
v0idFuncti0n <lakchouchanas@gmail.com>
2021-04-23 15:36:45 +0000

    Added email verification system ( Features::emailVerification() needs to be enable )

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,6 +11,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+// Implement MustVerifyEmail to verify with email
 class User extends Authenticatable
 {
     use HasApiTokens;
