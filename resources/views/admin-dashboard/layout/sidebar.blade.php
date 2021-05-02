87b184beb9ec39f9a89aa54cb57f6945c789d669
v0idFuncti0n <lakchouchanas@gmail.com>
2021-05-02 16:01:06 +0000

    logout, manage profile, profile photo are working in admin dashboard

--- resources/views/admin-dashboard/layout/sidebar.blade.php
+++ resources/views/admin-dashboard/layout/sidebar.blade.php
@@ -4,35 +4,28 @@
             <div class="nav-link">
                 <div class="user-wrapper">
                     <div class="profile-image">
-                        <img src="{{ url('assets/images/faces/face8.jpg') }}" alt="profile image">
+                        <img src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}">
                     </div>
                     <div class="text-wrapper">
-                        <p class="profile-name">Richard V.Welsh</p>
+                        <p class="profile-name">{{ Auth::user()->name }}</p>
                         <div class="dropdown" data-display="static">
                             <a href="#" class="nav-link d-flex user-switch-dropdown-toggler" id="UsersettingsDropdown"
                                data-toggle="dropdown" aria-expanded="false">
-                                <small class="designation text-muted">Manager</small>
+                                <small
+                                    class="designation text-muted">{{ Auth::user()->getRoleNames()->first() }}</small>
                                 <span class="status-indicator online"></span>
                             </a>
                             <div class="dropdown-menu" aria-labelledby="UsersettingsDropdown">
-                                <a class="dropdown-item p-0">
-                                    <div class="d-flex border-bottom">
-                                        <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                                            <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
-                                        </div>
-                                        <div
-                                            class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right">
-                                            <i class="mdi mdi-account-outline mr-0 text-gray"></i>
-                                        </div>
-                                        <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                                            <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item mt-2"> Manage Accounts </a>
+                                <a href="{{ route('profile.show') }}" class="dropdown-item"> Manage Profile </a>
+                                <a class="dropdown-item"> Manage Accounts </a>
                                 <a class="dropdown-item"> Change Password </a>
                                 <a class="dropdown-item"> Check Inbox </a>
-                                <a class="dropdown-item"> Sign Out </a>
+                                <form action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button class="dropdown-item"
+                                       onclick="e.preventDefault(); this.closest('form').submit();"> Sign Out </button>
+                                </form>
+
                             </div>
                         </div>
                     </div>
