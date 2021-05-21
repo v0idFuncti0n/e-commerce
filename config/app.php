92febdb97cfc88a57c764654d5cb8ed4881fc635
v0idFuncti0n <lakchouchanas@gmail.com>
2021-05-21 17:54:00 +0100

    fix: image/intervention not used

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
 
     ],
 
@@ -231,7 +230,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
