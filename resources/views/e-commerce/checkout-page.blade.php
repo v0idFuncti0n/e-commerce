cb8c36c69c155fb3a40060e7468d63811938790f
Pharaohsama <mohyieddine.saadani@etu.uae.ac.ma>
2021-07-13 21:44:48 +0100

    COPYRIGHT DELETED

--- resources/views/e-commerce/checkout-page.blade.php
+++ resources/views/e-commerce/checkout-page.blade.php
@@ -350,8 +350,7 @@
     <hr class="my-4">
     <!--Copyright-->
     <div class="footer-copyright py-3">
-        © 2019 Copyright:
-        <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> MDBootstrap.com </a>
+        <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank">  </a>
     </div>
     <!--/.Copyright-->
 
--- resources/views/e-commerce/home-page.blade.php
+++ resources/views/e-commerce/home-page.blade.php
@@ -376,8 +376,8 @@
 
     <!--Copyright-->
     <div class="footer-copyright py-3">
-        © 2019 Copyright:
-        <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> MDBootstrap.com </a>
+
+        <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank">  </a>
     </div>
     <!--/.Copyright-->
 
